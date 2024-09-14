e765ecea3e6e1a85fa017c70cd718b12d45e5f9c
Nicole Slater <nicoleslater@pursuit.org>
2024-09-14 10:57:24 -0400

    Updating Seed for functionality

--- db/seed.sql
+++ db/seed.sql
@@ -2,8 +2,8 @@
 
 INSERT INTO products (name, price, brand, model, description, condition)
 VALUES
-('2009 Fender Standard Stratocaster', 499, 'Fender', 'Standard Stratocaster with Rosewood Fretboard', 'This instrument sports a Rosewood neck, a Three-Tone Sunburst finish and weighs in at JUST under 8lbs.', 'Very Good (Used)'),
-('Eastwood Airline 2P with Rosewood Fretboard', 450, 'Eastwood', 'Airline 59 2P with Rosewood Fretboard', 'It has player wear and may need a few frets but it is still playable with no buzzing or fretting out. Electronics are working great.', 'Good (Used)'),
-('Suhr Classic S Surf Green Rosewood', 2950, 'Suhr', 'Suhr Classic S', 'Brand New items are sold by an authorized dealer or original builder and include all original packaging', 'Brand New'),
-('60th Anniversary Fender Deluxe Nashville Telecaster with Rosewood Fretboard 2005 Honey Blonde', 825, 'Fender', 'Deluxe Nashville Telecaster with Rosewood Fretboard', 'Very Good (Used)'),
-('Gibson SGJ 2013 - Rubbed Vintage Burst', 1000, 'Gibson', 'SGJ', 'The guitar features the iconic SG double-cutaway shape, providing easy access to the higher frets.', 'Very Good (Used)');
+('2009 Fender Standard Stratocaster', 499, 'Fender', 'Standard Stratocaster', 'This instrument sports a Rosewood neck, a Three-Tone Sunburst finish and weighs in at JUST under 8lbs.', 'Very Good (Used)'),
+('Eastwood Airline 2P', 450, 'Eastwood', 'Airline 59 2P', 'It has player wear and may need a few frets but it is still playable with no buzzing or fretting out.', 'Good (Used)'),
+('Suhr Classic S Surf Green Rosewood', 2950, 'Suhr', 'Suhr Classic S', 'Brand New items are sold by an authorized dealer or original builder.', 'Brand New'),
+('Gibson Kirk Douglas Signature', 2450, 'Gibson', 'Kirk Douglas Signature SG','There is normal wear on the pickguard.', 'Good (Used)'),
+('Gibson SGJ 2013', 1000, 'Gibson', 'SGJ', 'The guitar features the iconic SG double-cutaway shape, providing easy access to the higher frets.', 'Very Good (Used)');
