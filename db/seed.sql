3f330ac8cada5fe2f6c0721c163a245376eeaf56
Nicole Slater <nicoleslater@pursuit.org>
2024-09-14 10:58:23 -0400

    Updating Seed for functionality

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO products (name, price, brand, model, description, condition)
 VALUES
-('2009 Fender Standard Stratocaster', 499, 'Fender', 'Standard Stratocaster', 'This instrument sports a Rosewood neck, a Three-Tone Sunburst finish and weighs in at JUST under 8lbs.', 'Very Good (Used)'),
+('2009 Fender Standard Stratocaster', 499, 'Fender', 'Standard Stratocaster', 'This instrument sports a Rosewood neck, a Three-Tone Sunburst finish.', 'Very Good (Used)'),
 ('Eastwood Airline 2P', 450, 'Eastwood', 'Airline 59 2P', 'It has player wear and may need a few frets but it is still playable with no buzzing or fretting out.', 'Good (Used)'),
 ('Suhr Classic S Surf Green Rosewood', 2950, 'Suhr', 'Suhr Classic S', 'Brand New items are sold by an authorized dealer or original builder.', 'Brand New'),
 ('Gibson Kirk Douglas Signature', 2450, 'Gibson', 'Kirk Douglas Signature SG','There is normal wear on the pickguard.', 'Good (Used)'),
